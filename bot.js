c3c5e6331422a164ed74504debcac5afec4bd97a
daliang3 <friedricesushi@hotmail.com>
2017-07-11 16:26:57 -0700

    test

--- bot.js
+++ bot.js
@@ -1,24 +1,14 @@
 // Requires 
-const commando = require('discord.js-commando');
+const commando = require('discord.js');
 const path = require('path');
 const {BOT_TOKEN, OWNER} = process.env; 
 
 // Create an instance of a Commando client
-const client = new commando.Client({
-    commandPrefix: './',
-    owner: OWNER
-});
-
-client.registry
-        .registerGroups([
-            ['pad', 'Puzzles and Waifus']
-        ])
-        .registerDefaults()
-        .registerCommandsIn(path.join(__dirname, 'commands'));
+const client = new commando.Client();
 
 client.on('ready', () => {
     console.log('Logged in!');
-    client.user.setGame(':thinking:');
+    client.user.setGame(':sigh:');
 })
 
 client.login(BOT_TOKEN);
